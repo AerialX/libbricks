e0518265590a12759e6e5213ddbcea48d1ac1771
Aaron Lindsay <aaron@aaronlindsay.com>
2011-09-06 19:41:13 -0400

    libpng option check rearrangement.

--- include/bricks/imaging/png.h
+++ include/bricks/imaging/png.h
@@ -1,14 +1,18 @@
 #pragma once
 
+#include "bricks/config.h"
+
+#ifdef BRICKS_CONFIG_IMAGING_LIBPNG
+
 #include "bricks/imaging/image.h"
 #include "bricks/io/filesystem.h"
 #include "bricks/io/filestream.h"
 
 namespace Bricks { namespace Imaging {
-#ifdef BRICKS_CONFIG_IMAGING_LIBPNG
 	namespace PNG {
 		ReturnPointer<Image> LoadImage(const Pointer<IO::Stream>& stream, bool transform = false);
 		static inline ReturnPointer<Image> LoadImage(const String& path, const Pointer<IO::Filesystem>& filesystem = NULL, bool transform = false) { return LoadImage(autonew IO::FileStream(path, IO::FileOpenMode::Open, IO::FileMode::ReadOnly, IO::FilePermissions::OwnerReadWrite, filesystem), transform); }
 	}
-#endif
 } }
+
+#endif
