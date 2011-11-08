a330b6ca5cbd3d4c4f0e574f55a765456fd43924
Aaron Lindsay <aaron@aaronlindsay.com>
2011-11-07 19:30:15 -0500

    Adjust a release ifdef

--- include/bricks/object.h
+++ include/bricks/object.h
@@ -278,10 +278,8 @@ namespace Bricks {
 	inline void* Object::mallocthrowable(size_t size)
 	{
 		void* data = malloc(size);
-#ifndef BRICKS_FEATURE_RELEASE
 		if (!data)
-			throw OutOfMemoryException();
-#endif
+			BRICKS_FEATURE_RELEASE_THROW_FATAL(OutOfMemoryException());
 		return data;
 	}
 	template<typename T> template<typename U> inline Pointer< T >::Pointer(const U& t, typename SFINAE::EnableIf<!SFINAE::IsConst<U>::Value && SFINAE::IsSameType<T, U>::Value>::Type* dummy) : value(&const_cast<U&>(t)) { throw InvalidArgumentException(); }
