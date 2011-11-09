7c3f72b0dc2c95f489a3118e7e447cc77df00838
Aaron Lindsay <aaron@aaronlindsay.com>
2011-11-09 18:49:04 -0500

    Fix some Data bugs

--- include/bricks/internal/data.h
+++ include/bricks/internal/data.h
@@ -10,15 +10,15 @@ namespace Bricks {
 		size_t length;
 		bool owned;
 
+		void Construct() { if (data && owned) delete[] data; data = new u8[length]; owned = true; }
+
 	public:
-		Data(size_t length) : length(length) { Construct(); }
+		Data(size_t length) : data(NULL), length(length), owned(true) { Construct(); }
 		Data(const void* data, size_t length, bool copy = true) : data(copy ? NULL : (u8*)data), length(length), owned(!copy) { if (copy) { Construct(); CopyFrom(data, length); } }
 		Data(const String& string, bool copy = true) : data(copy ? NULL : (u8*)string.GetBuffer()), length(string.GetLength()), owned(!copy) { if (copy) { Construct(); CopyFrom(string.GetBuffer(), string.GetLength()); } }
 		Data(const Data& data, bool copy = true) : data(copy ? NULL : (u8*)data.GetData()), length(data.GetLength()), owned(!copy) { if (copy) { Construct(); CopyFrom(data); } }
 
 		~Data() { if (data && owned) delete[] data; }
-
-		void Construct() { if (data && owned) delete[] data; data = new u8[length]; owned = true; }
 
 		void CopyFrom(const void* value, size_t len, size_t offset = 0) { memcpy(data + offset, value, BRICKS_FEATURE_MIN(len, length - offset)); }
 		void CopyFrom(const Data& value, size_t offset = 0) { memcpy(data + offset, value.GetData(), BRICKS_FEATURE_MIN(value.GetLength(), length - offset)); }
@@ -26,6 +26,7 @@ namespace Bricks {
 		Data& operator=(const Data& rhs) { length = rhs.GetLength(); Construct(); CopyFrom(rhs); return *this; }
 
 		size_t GetLength() const { return length; }
+		size_t GetSize() const { return length; }
 		const void* GetData() const { return data; }
 		void* GetData() { return data; }
 		operator const void*() const { return data; }
@@ -33,6 +34,6 @@ namespace Bricks {
 		u8& operator[](size_t index) { return data[index]; }
 		const u8& operator[](size_t index) const { return data[index]; }
 
-		ReturnPointer<Data> Subdata(size_t offset = 0, size_t length = String::npos) const { return autonew Data(*this); }
+		ReturnPointer<Data> Subdata(size_t offset = 0, size_t length = String::npos) const { return autonew Data(data + offset, length == String::npos ? this->length : length); }
 	};
 }
