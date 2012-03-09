0b09939e48fefd910bae9ff9f20669dfb0fb119f
Aaron Lindsay <aaron@aaronlindsay.com>
2012-03-08 21:26:28 -0500

    Implement Time::GetCurrentTime() with gettimeofday() when necessary.

--- include/bricks/core/time.h
+++ include/bricks/core/time.h
@@ -3,6 +3,7 @@
 #include "bricks/core/types.h"
 
 struct timespec;
+struct timeval;
 
 namespace Bricks {
 	class Timespan;
@@ -19,6 +20,7 @@ namespace Bricks {
 		Time() : seconds(0), nanoseconds(0) { }
 		Time(long seconds, long nanoseconds = 0) : seconds(seconds), nanoseconds(nanoseconds) { }
 		Time(const struct timespec& spec);
+		Time(const struct timeval& val);
 
 		static Time GetCurrentTime();
 
@@ -26,6 +28,7 @@ namespace Bricks {
 		long GetNanoseconds() const { return nanoseconds; }
 
 		struct timespec GetTimespec() const;
+		struct timeval GetTimeval() const;
 
 		Time operator +(const Timespan& span) const;
 		Time operator -(const Timespan& span) const;
