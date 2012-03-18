ec531f7a8b7d275a9cc4d5f26a6abd51bf7702db
Aaron Lindsay <aaron@aaronlindsay.com>
2012-03-18 01:41:55 -0400

    Move most of MemoryStream to its own source file

--- include/bricks/io/memorystream.h
+++ include/bricks/io/memorystream.h
@@ -1,10 +1,6 @@
 #pragma once
 
-#include "bricks/core/math.h"
 #include "bricks/io/stream.h"
-
-#include <stdlib.h>
-#include <string.h>
 
 namespace Bricks { namespace IO {
 	class MemoryStream : public Stream
@@ -18,41 +14,27 @@ namespace Bricks { namespace IO {
 		static const int BlockSize = 0x400;
 
 	public:
-		MemoryStream() : data(NULL), length(0), position(0), allocated(0) { }
-		MemoryStream(unsigned long size) : data(NULL), length(0), position(0), allocated(0) { Allocate(size); }
-		MemoryStream(const MemoryStream& stream) : data(NULL), length(stream.length), allocated(0) { Allocate(length); memcpy(data, stream.data, length); }
-		MemoryStream(const Data& data) : data(NULL), length(data.GetLength()), allocated(0) { Allocate(length); memcpy(this->data, data.GetData(), length); }
-		MemoryStream(const void* data, unsigned long length) : data(NULL), length(length), allocated(0) { Allocate(length); memcpy(this->data, data, length); }
-		~MemoryStream() { free(data); }
+		MemoryStream();
+		MemoryStream(unsigned long size);
+		MemoryStream(const MemoryStream& stream);
+		MemoryStream(const Data& data);
+		MemoryStream(const void* data, unsigned long length);
+		~MemoryStream();
 
-		MemoryStream& operator =(const MemoryStream& stream) { length = stream.length; Allocate(length); memcpy(data, stream.data, length); return *this; }
+		MemoryStream& operator =(const MemoryStream& stream);
 
-		void Allocate(unsigned long size) {
-			allocated = Math::RoundUp(size, BlockSize);
-			data = (u8*)realloc(data, allocated);
-		}
+		void Allocate(unsigned long size);
 
-		size_t Read(void* buffer, size_t size) {
-			size = Math::Min(size, length - position);
-			memcpy(buffer, data + position, size);
-			position += size;
-			return size;
-		}
+		size_t Read(void* buffer, size_t size);
 
-		size_t Write(const void* buffer, size_t size) {
-			if (position + size > length)
-				SetLength(position + size);
-			memcpy(data + position, buffer, size);
-			position += size;
-			return size;
-		}
+		size_t Write(const void* buffer, size_t size);
 
-		u64 GetLength() const { return length; }
-		void SetLength(u64 length) { this->length = length; Allocate(length); }
-		u64 GetPosition() const { return position; }
-		void SetPosition(u64 position) { this->position = position; }
-		int ReadByte() { return data[position++]; }
+		void SetLength(u64 length);
+		void SetPosition(u64 position);
+		int ReadByte();
 
 		void* GetBuffer() { return data; }
+		u64 GetLength() const { return length; }
+		u64 GetPosition() const { return position; }
 	};
 } }
