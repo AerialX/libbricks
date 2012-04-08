8510cf5d71211e8bd240f95ffbcc406e9ca1b4e3
Aaron Lindsay <aaron@aaronlindsay.com>
2012-04-08 18:23:46 -0400

    Add a deep pointer comparison class

--- include/bricks/collections/comparison.h
+++ include/bricks/collections/comparison.h
@@ -2,6 +2,7 @@
 
 #include "bricks/core/sfinae.h"
 #include "bricks/core/object.h"
+#include "bricks/core/autopointer.h"
 
 namespace Bricks { namespace Collections {
 	namespace ComparisonResult {
@@ -46,5 +47,16 @@ namespace Bricks { namespace Collections {
 	{
 	public:
 		ComparisonResult::Enum Compare(const T& v1, const T& v2) { if (&v1 == &v2) return ComparisonResult::Equal; return ComparisonResult::Less; }
+	};
+
+	template<typename T>
+	class PointerValueComparison : public ValueComparison<T*>
+	{
+	protected:
+		AutoPointer<ValueComparison<T> > comparison;
+
+	public:
+		PointerValueComparison(ValueComparison<T>* comparison = autonew OperatorValueComparison<T>()) : comparison(comparison) { }
+		ComparisonResult::Enum Compare(T*const& v1, T*const& v2) { return comparison->Compare(*v1, *v2); }
 	};
 } }
