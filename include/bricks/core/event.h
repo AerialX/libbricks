8d9dfb309dba7beba6fd4b7244a7bdd4ffe84c0d
Aaron Lindsay <aaron@aaronlindsay.com>
2013-11-15 20:01:39 -0500

    Use listguard for events

--- include/bricks/core/event.h
+++ include/bricks/core/event.h
@@ -2,6 +2,9 @@
 
 #include "bricks/core/delegate.h"
 
+#include "bricks/collections/autoarray.h"
+#include "bricks/collections/listguard.h"
+
 #define BRICKS_ARGLIST_HEADER "bricks/core/event_internal.h"
 #include "bricks/core/arglist.h"
 #undef BRICKS_ARGLIST_HEADER
--- include/bricks/core/event_internal.h
+++ include/bricks/core/event_internal.h
@@ -2,8 +2,6 @@
 #error Use event.h
 #endif
 
-#include "bricks/collections/autoarray.h"
-
 namespace Bricks {
 	template<typename R BRICKS_ARGLIST_COMMA BRICKS_ARGLIST_TYPENAMES> class Event<R(BRICKS_ARGLIST_TYPES)> : public Delegate<void(BRICKS_ARGLIST_TYPES)>
 	{
@@ -15,37 +13,43 @@ namespace Bricks {
 		{
 		public:
 			Collections::ComparisonResult::Enum Compare(EventItem*const& v1, EventItem*const& v2) {
-				typedef const Internal::MethodFunctionBase<R(BRICKS_ARGLIST_TYPES)>* delegate;
-				delegate d1 = dynamic_cast<delegate>(v1->function.GetValue());
-				if (d1) {
-					delegate d2 = dynamic_cast<delegate>(v2->function.GetValue());
-					if (d2)
-						return d1->pointer == d2->pointer ? Collections::ComparisonResult::Equal : Collections::ComparisonResult::Less;
-				}
-				return *v1 == *v2 ? Collections::ComparisonResult::Equal : Collections::ComparisonResult::Less;
+				return (v1 == v2 || *v1 == *v2) ? Collections::ComparisonResult::Equal : Collections::ComparisonResult::Less;
 			}
 		};
 
 		typedef Collections::AutoArray<EventItem> EventItemList;
 		EventItemComparison comparison;
-		EventItemList list;
+		EventItemList listInternal;
+		Collections::ListGuard<EventItemList> list;
 
 	public:
-		Event() : list(tempnew comparison) { }
-		Event(const Event& event) : list(event.list, tempnew comparison) { }
+		Event() : listInternal(tempnew comparison), list(tempnew listInternal) { }
+		Event(const Event& event) : listInternal(event.listInternal, tempnew comparison), list(tempnew listInternal) { }
 
-		Event& operator =(const Event& event) { list = EventItemList(event.list, tempnew comparison); return *this; }
+		Event& operator =(const Event& event) { listInternal = EventItemList(event.listInternal, tempnew comparison); return *this; }
 
 		Event& operator +=(EventItem* delegate) { list.AddItem(delegate); return *this; }
 		Event& operator +=(const EventItem& delegate) { list.AddItem(autonew EventItem(delegate)); return *this; }
 
 		Event& operator -=(EventItem* delegate) { list.RemoveItems(delegate); return *this; }
 		Event& operator -=(const EventItem& delegate) { list.RemoveItems(tempnew delegate); return *this; }
-		template<typename T> Event& operator -=(const T& object) { return operator-=(tempnew object); }
-		template<typename T> Event& operator -=(T* object) { list.RemoveItems(tempnew EventItem(static_cast<Internal::BaseDelegate<R(BRICKS_ARGLIST_TYPES)>*>(tempnew Internal::MethodFunctionBase<R(BRICKS_ARGLIST_TYPES)>(static_cast<void*>(object))))); return *this; }
+
+		Event& operator -=(const Object* object) {
+			BRICKS_FOR_EACH (EventItem* item, listInternal) {
+				typedef const Internal::MethodFunctionBase<R(BRICKS_ARGLIST_TYPES)> FunctionDelegate;
+				FunctionDelegate* d1 = CastToDynamic<FunctionDelegate>(item->function);
+				if (d1 && d1->pointer == object) {
+					list.RemoveItem(item);
+					return operator-=(object);
+				}
+			}
+			return *this;
+		}
 
 		operator bool() const { return list.GetCount(); }
 
-		void operator ()(BRICKS_ARGLIST_TYPES_NAMES) { foreach (EventItem* item, list) (*item)(BRICKS_ARGLIST_ARGS); }
+		void operator ()(BRICKS_ARGLIST_TYPES_NAMES) { BRICKS_FOR_EACH (EventItem* item, list) (*item)(BRICKS_ARGLIST_ARGS); }
+
+		bool IsCalling() const { return list.IsGuarding(); }
 	};
 }
